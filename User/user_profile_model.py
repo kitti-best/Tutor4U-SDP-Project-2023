9166bcd42cc015dc8cc6f4bc28577ff20d19e579
Schmaew <109778280+Schmaew@users.noreply.github.com>
2023-09-10 02:31:17 +0700

    Update user_profile_model.py
    update regex phone number

--- User/user_profile_model.py
+++ User/user_profile_model.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 import uuid
 
 class UserProfile(models.Model):
@@ -10,9 +10,9 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(validators=[RegexValidator(regex="((\+66|0)(\d{1,2}\-?\d{3}\-?\d{3,4}))|((\+๖๖|๐)([๐-๙]{1,2}\-?[๐-๙]{3}\-?[๐-๙]{3,4}))")])
     date_joined = models.DateTimeField(auto_now_add=True)
-    contact_number = models.IntegerField(max_length=10, blank=True, null=True)
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     is_tutor = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
@@ -25,7 +25,3 @@ class UserProfile(models.Model):
 
     def create_tutor(self):
         self.is_tutor = True
-
-
-
-    
