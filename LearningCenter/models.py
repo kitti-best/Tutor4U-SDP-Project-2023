77ec32e5eaa380ff55ffc0580b5f55a9e47be12b
Schmaew <109778280+Schmaew@users.noreply.github.com>
2023-09-09 20:58:04 +0700

    Update models.py
    learning center model

--- LearningCenter/models.py
+++ LearningCenter/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models as gis_models
+import uuid
 
-# Create your models here.
+class LearningCenter(models.Model):
+    _uuid = models.UUIDField(default=uuid.uuid4, db_index=True, editable=False, primary_key=True, unique=True)
+    name = models.CharField(validators=[MinLengthValidator(4)], max_length=150, unique=True)
+    description = models.TextField()
+    location = gis_models.PointField()
+    address = models.CharField(max_length=255, blank=True)
+    website = models.URLField(blank=True, null=True)
+    phone_number = models.IntegerField(max_length=10, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who owns the learning center
+    subjects_taught = models.CharField(max_length=255, blank=False)
+    popularity = models.IntegerField(max_length=30)
+
+    def __str__(self):
+        return(f"{self.name}")
