1f2a4c8948509bb26b1a8662b5c402bc4e6dbb37
Schmaew <109778280+Schmaew@users.noreply.github.com>
2023-09-10 02:30:36 +0700

    Update models.py
    add regex phone number and etc

--- LearningCenter/models.py
+++ LearningCenter/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.contrib.postgres.fields import ArrayField
 import uuid
 
 class LearningCenter(models.Model):
@@ -11,16 +13,17 @@ class LearningCenter(models.Model):
     longtitude = models.FloatField(max_length=15)
     house_number = models.CharField(max_length=15) # เลขที่บ้าน
     section = models.CharField(max_length=10) # หมู่บ้าน
+    street = models.CharField(max_length=15) # ถนน
     sub_district = models.CharField(max_length=30) # ตำบล
     district = models.CharField(max_length=30) # อำเภอ
     province = models.CharField(max_length=30) # จังหวัด
     country = models.CharField(max_length=30, default="Thailand")
     website = models.URLField(blank=True, null=True)
-    phone_number = models.IntegerField(max_length=10, blank=True, null=True)
+    phone = models.CharField(validators=[RegexValidator(regex="((\+66|0)(\d{1,2}\-?\d{3}\-?\d{3,4}))|((\+๖๖|๐)([๐-๙]{1,2}\-?[๐-๙]{3}\-?[๐-๙]{3,4}))")])
     email = models.EmailField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who owns the learning center
-    subjects_taught = models.CharField(max_length=255, blank=False)
+    subjects_taught = ArrayField(models.CharField(max_length=255, blank=False)) 
     popularity = models.IntegerField(default=0)
 
     def __str__(self):
