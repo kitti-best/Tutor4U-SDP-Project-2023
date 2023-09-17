33ec128803897a532e9595662e57e134cb3f09a3
Roshanen <numesattahip@hotmail.com>
2023-09-17 17:03:47 +0700

    <feat> manage search filter

--- LearningCenter/views.py
+++ LearningCenter/views.py
@@ -4,26 +4,47 @@ from rest_framework import status
 from .models import LearningCenter
 from .serializers import LearningCenterSerializer
 from abc import ABC, abstractmethod
+from django.db.models import Q
 
 class ManageLearningCenter(APIView):
     def post(self, request):
         serializer = LearningCenterSerializer(data=request.data)
 
-        print("something!")
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SearchLearningCenter(APIView, ABC):
-    @abstractmethod
-    def get(self, request):
-        pass
-class SearchNameLearningCenter(SearchLearningCenter):
     def get(self, request):
         name = self.request.query_params.get('name', '')
-        
-        learning_centers = LearningCenter.objects.filter(name__icontains=name)
-        serializer = LearningCenterSerializer(learning_centers, many=True)
+        ratings = self.request.query_params.get('rating', '').split(',')
+        levels = self.request.query_params.get('level', '').split(',')
+        subjects_taught = self.request.query_params.get('subjects_taught', '').split(',')
+
+        result_learning_centers = self.search_learning_centers(name, ratings, levels, subjects_taught)
+        serializer = LearningCenterSerializer(result_learning_centers, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def search_learning_centers(self, name, ratings, levels, subjects_taught):
+        # Query use for complex queries
+        query = Q()
+
+        if name:
+            query |= Q(name__icontains=name)
+
+        for rating in ratings:
+            query &= Q(rating__icontains=rating.strp())
+
+        for level in levels:
+            query &= Q(levels__icontains=level.strip())
+
+        for subject in subjects_taught:
+            query &= Q(subjects_taught__icontains=subject.strip())
+            
+        query &= Q(status='approve')
+
+        queryset = LearningCenter.objects.filter(query).order_by('-popularity')
+
+        return queryset
