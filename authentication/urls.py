528699530b76c8cf40601540b56c82e3b0f9d790
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2023-09-17 19:48:05 +0700

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view()), 
     path('token/generate', TokenObtainPairView.as_view()),
     path('token/verify/', TokenVerifyView.as_view()),
-    path('activate/<str:uidb64>/<str:token>/', views.EmailActivation.as_view())
+    path('activate/<str:uidb64>/<str:token>/', views.EmailActivation.as_view(), name='activate'),
+    path('reset-password-sender/<str:email>/', views.ResetPasswordSender.as_view()),
+    path('reset-password/<str:uidb64>/<str:token>/<str:password>/', views.ResetPassword.as_view(), name='reset-password')
 ]
