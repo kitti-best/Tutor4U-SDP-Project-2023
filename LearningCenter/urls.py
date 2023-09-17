47225111837a815f520892186f724221e3fe540a
Kittipan Onlay <61462393+kitti-best@users.noreply.github.com>
2023-09-17 19:55:15 +0700

    <feat>: add url for learning center

--- LearningCenter/urls.py
+++ LearningCenter/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('learningcenters/', views.ManageLearningCenter.as_view(), name='manage-learning-center'),
     path('search/', views.SearchLearningCenter.as_view(), name='search-learning-center'),
-    path('change-status/<str:name>/<str:status>/', views.ChangeLearningCenterStatus.as_view())
+    path('change-status/<str:name>/<str:status>/', views.ChangeLearningCenterStatus.as_view()),
+    path("view/<id>", views.ViewLearningCenterInformation.as_view(), name="view"),
 ]
