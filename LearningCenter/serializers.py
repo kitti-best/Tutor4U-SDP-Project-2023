b9eb0b3b9f6d1590a161b8ed853c17d221d78841
best <kittibest46@gmail.com>
2023-09-22 20:44:30 +0700

    <feat>: add serializer for getting student and tutor

--- LearningCenter/serializers.py
+++ LearningCenter/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import LearningCenter
 
+
 class LearningCenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = LearningCenter
         fields = '__all__'
+
+
+class LearningCenterStudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LearningCenter
+        fields = ('students')
+
+
+class LearningCenterTutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LearningCenter
+        fields = ('tutor')
