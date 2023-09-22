578c44314a77374bb9e32f1d3b899dd04e9918a2
best <kittibest46@gmail.com>
2023-09-22 20:45:36 +0700

    <feat>: change view learning center to by name instead of ID

--- LearningCenter/urls.py
+++ LearningCenter/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('learningcenters/', views.ManageLearningCenter.as_view(), name='manage-learning-center'),
     path('search/', views.SearchLearningCenter.as_view(), name='search-learning-center'),
     path('change-status/<str:name>/<str:status>/', views.ChangeLearningCenterStatus.as_view()),
-    path("view/<id>", views.ViewLearningCenterInformation.as_view(), name="view"),
+    path("view/<name>", views.ViewLearningCenterInformation.as_view(), name="view"),
+    path("add_student", views.AddStudentToLearningCenter.as_view(), name="add_student"),
 ]
