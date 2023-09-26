f8bc0df4249c265dabf2581b9dabd5fbcec327cc
Kachen <106764256+Roshanen@users.noreply.github.com>
2023-09-26 18:59:40 +0700

    <feat> find learning_centers in range

--- LearningCenter/views.py
+++ LearningCenter/views.py
@@ -6,6 +6,7 @@ from .serializers import LearningCenterSerializer
 from abc import ABC, abstractmethod
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
+import math
 
 
 class ViewLearningCenterInformation(APIView):
@@ -74,6 +75,56 @@ class SearchLearningCenter(APIView, ABC):
         queryset = LearningCenter.objects.filter(query).order_by("-popularity")
 
         return queryset
+
+class LearningCenterDistanceFilter(APIView):
+    def get(self, request):
+        # Get the user's latitude and longitude from the request (you can customize this part)
+        user_latitude = float(request.query_params.get('lat'))
+        user_longitude = float(request.query_params.get('lon'))
+        
+        # Maximum distance in kilometers
+        max_distance_km = float(request.query_params.get('max_distance', 5))
+
+        # Filter Learning Centers within the specified distance
+        learning_centers = self.filter_learning_centers_in_distance(
+            user_latitude, user_longitude, max_distance_km
+        )
+
+        # Serialize and return the filtered Learning Centers
+        serializer = LearningCenterSerializer(learning_centers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def filter_learning_centers_in_distance(self, lat, lon, max_distance_km=5):
+        learning_centers = LearningCenter.objects.all()
+        filtered_centers = []
+
+        user_location = (lat, lon)
+
+        for center in learning_centers:
+            center_location = (center.latitude, center.longitude)
+            distance = self.vector_distance(*user_location, *center_location)
+
+            if distance <= max_distance_km:
+                filtered_centers.append(center)
+
+        return filtered_centers
+
+    def vector_distance(self, lat1, lon1, lat2, lon2):
+        earth_radius = 6371.0
+        lat1_rad = lat1 * (3.14159265358979323846 / 180.0)
+        lon1_rad = lon1 * (3.14159265358979323846 / 180.0)
+        lat2_rad = lat2 * (3.14159265358979323846 / 180.0)
+        lon2_rad = lon2 * (3.14159265358979323846 / 180.0)
+
+        d_lat = lat2_rad - lat1_rad
+        d_lon = lon2_rad - lon1_rad
+
+        a = (math.sin(d_lat / 2))**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * (math.sin(d_lon / 2))**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        distance = earth_radius * c
+
+        return distance
 
 
 class ChangeLearningCenterStatus(APIView):
