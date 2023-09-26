e81eedc6ee9b7950c06938b2be5cc9c5301b64ad
Peqchji <110451238+Peqchji@users.noreply.github.com>
2023-09-27 02:05:14 +0700

    <hotfix> cookies secure config

--- authentication/urls.py
+++ authentication/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('sign-out/', views.LogoutAPIViews.as_view()), 
     path('sign-out/all/', views.LogoutAllAPIViews.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()), 
-    path('token/generate/', TokenObtainPairView.as_view()),
     path('token/verify/', TokenVerifyView.as_view()),
     path('activate/<str:uidb64>/<str:token>/', views.EmailActivation.as_view(), name='activate'),
     path('reset-password-sender/', views.ResetPasswordSender.as_view()),
