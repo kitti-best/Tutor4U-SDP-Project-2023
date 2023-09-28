c312c68b4b81f91d2840f9c0d0b7e357d1a7f440
best <kittibest46@gmail.com>
2023-09-28 21:18:47 +0700

    disable debug

--- KMITLWebAppClassProject/settings.py
+++ KMITLWebAppClassProject/settings.py
@@ -18,7 +18,8 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('NODE_ENV') == "dev"
+# DEBUG = env('NODE_ENV') == "dev"
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
