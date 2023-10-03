08c97c4c368825b943406565224160c7509128bf
best <kittibest46@gmail.com>
2023-10-03 22:21:02 +0700

    <add>: make get default image a static

--- Images/models.py
+++ Images/models.py
@@ -6,12 +6,15 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+
 class Images(models.Model):
     image_id = models.UUIDField(default=uuid.uuid4, db_index=True, editable=False, primary_key=True, unique=True)
     image_file = models.ImageField(upload_to='images/', height_field=None, width_field=None, default='')
-    
+
+    @staticmethod
     def get_default_image():
         default_image = Images.objects.filter(image_id='9a4b5d5d-01bd-493c-a143-2f8305dd2b35').first().image_id
+        print(default_image, type(default_image))
         return default_image
     
     def get_image_url(self):
