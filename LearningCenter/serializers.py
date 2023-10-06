828ad69c21b8ad05ba0b115a2965a367d8f1e004
Peqchji <110451238+Peqchji@users.noreply.github.com>
2023-10-06 22:41:09 +0700

    <refactor> LearningCenter create method

--- LearningCenter/serializers.py
+++ LearningCenter/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 
+from django.shortcuts import get_object_or_404
+
 from Locations.serializers import LocationsSerializer
-from .models import LearningCenter, Tutor, Student, Locations, Subjects, LearningCenterLevels, SubjectsTaught, Levels
+from .models import LearningCenter, Tutor, Student, Locations, Subjects, SubjectsTaught, Levels, LearningCenterLevels
+
+
+
+class SubjectsTaughtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectsTaught
+        fields = '__all__'
 
 class LearningCenterInfoSerializer(serializers.ModelSerializer):
     location = LocationsSerializer()
@@ -12,33 +21,38 @@ class LearningCenterInfoSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         location_data = validated_data.pop('location')
-        learning_center_levels_data = validated_data.pop('learning_center_levels', [])
-        subjects_taught_data = validated_data.pop('subjects_taught', [])
-
+        levels = validated_data.pop('learning_center_levels', [])
+        subjects_taught = validated_data.pop('subjects_taught', [])
+        
         location = Locations.objects.create(**location_data)
         learning_center = LearningCenter.objects.create(location=location, **validated_data)
-
-        for subject_data in subjects_taught_data:
+        
+        for subject_data in subjects_taught:
             subject_name = subject_data['subject']
-            subject = Subjects.objects.get(subject_name=subject_name)
+            subject = get_object_or_404(Subjects, subject_name=subject_name)
             SubjectsTaught.objects.create(learning_center=learning_center, subject=subject)
 
-        for level_data in learning_center_levels_data:
+        for level_data in levels:
             level_name = level_data['level']
-            level = Levels.objects.get(level_name=level_name)
+            level = get_object_or_404(Levels, level_name=level_name)
             LearningCenterLevels.objects.create(learning_center=learning_center, level=level)
-
-        return learning_center
-
-    def data(self, learning_center):
-        data = super().data
         
+        return learning_center
+    
+    @property
+    def data(self):
+        data = super().data
+        learning_center = self.instance
+
         tutors = learning_center.tutor_set.all()
         students = learning_center.student_set.all()
+        
         thumbnail = learning_center.thumbnail.get_image_url()
         subjects_taught = learning_center.subjectstaught_set.all()
-        
+        levels = learning_center.learningcenterlevels_set.all()
+
         self.get_subjects_taught(subjects_taught, data)
+        self.get_levels(levels, data)
         data.update({'thumbnail': thumbnail})
         data.update({'tutors': self.get_profile(tutors)})
         data.update({'students': self.get_profile(students)})
@@ -53,11 +67,23 @@ class LearningCenterInfoSerializer(serializers.ModelSerializer):
     
     def get_subjects_taught(self, subjects_data, response):
         subjects_taught_list = []
+        subject_thumbnails = []
         for subject_taught in subjects_data:
             subject = subject_taught.subject
-            subjects_taught_list.append(subject.get_subject())
+            subjects_taught_list.append(subject.subject_name)
+            subject_thumbnails.append(subject.image.get_image_url())
+        
         response.update({'subjects_taught': subjects_taught_list})
+        response.update({'subject_thumbnails': subject_thumbnails})
         return response
+    
+    def get_levels(self, levels_data, response):
+        level_list = []
+        for lc_level in levels_data:
+            level = lc_level.level
+            level_list.append(level.level_name)
+        response.update({'levels': level_list})
+        return level_list
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -71,8 +97,3 @@ class TutorSerializer(serializers.ModelSerializer):
         model = Tutor
         fields = '__all__'
 
-
-class SubjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subjects
-        fields = '__all__'
